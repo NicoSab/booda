8347c2423ff767852b4a8088c9f8f8fc8a9b19c2
Julien <jcze91@gmail.com>
2014-05-09 19:47:15 +0200

    welcome page

--- application/views/Inscription.php
+++ application/views/Inscription.php
@@ -1,9 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html;charset=utf-8"> 
+	<title>Booda : Peace & Love</title>
+	<link rel="stylesheet" href="<?php echo asset_url('style.css'); ?>" type="text/css">
+</head>
+<body  class="page_homepage plain">
+	<div class="header" style="background: url(<?php echo asset_url('img/header-bg.png'); ?>) repeat-x 0 100% #fff;">
+		<div class="center">
+			<div id="logo">
+				<img src="<?php echo asset_url('img/logo.png'); ?>" class="logoimg" width="100" height="29">
+			</div>
+			<div class="submit-buttons fl_r" id="join">
+				<span class="btn-or">
+					Déjà membre ?</span>
+					<a href="#" class="btn btn--orange">
+						Identifie-toi
+					</a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
 
-<head></head>
-
-<body>
 
 	<form method="post" action="">
 		<label for="connexion">Deja inscrit ?</label>
