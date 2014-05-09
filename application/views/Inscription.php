16099fb2d8a34babe1c9a72d9b04eacb8cc4b373
Julien <jcze91@gmail.com>
2014-05-09 23:14:39 +0200

    inscription page

--- application/views/Inscription.php
+++ application/views/Inscription.php
@@ -8,67 +8,96 @@
 <body  class="page_homepage plain">
 	<div class="header" style="background: url(<?php echo asset_url('img/header-bg.png'); ?>) repeat-x 0 100% #fff;">
 		<div class="center">
-			<div id="logo">
-				<img src="<?php echo asset_url('img/logo.png'); ?>" class="logoimg" width="100" height="29">
-			</div>
-			<div class="submit-buttons fl_r" id="join">
-				<span class="btn-or">
-					Déjà membre ?</span>
-					<a href="#" class="btn btn--orange">
-						Identifie-toi
-					</a>
-				</div>
+		<div id="logo">
+			<img src="<?php echo asset_url('img/logo.png'); ?>" class="logoimg" width="100" height="29">
+		</div>
+		<div class="submit-buttons fl_r" id="join">
+			<span class="btn-or">
+				Déjà membre ?</span>
+				<a href="#" class="btn btn--orange">
+					Identifie-toi
+				</a>
 			</div>
 		</div>
-	</body>
-</html>
-
-
-	<a href="connexion">Connectez-vous !</a>
-
-	<form method="post" action="">
-		<label for="pseudo">Pseudo : </label>
-		<input type="text" name="pseudo" value="<?php echo set_value('pseudo'); ?>" />
-		<?php echo form_error('pseudo'); ?>
-		</br>
-
-		<label for="mdp">Mot de passe :</label>
-		<input type="password" name="mdp" value="" />
-		<?php echo form_error('mdp'); ?>
-		</br>
-
-		<label for="name">Nom :</label>
-		<input type="text" name="name" value="" />
-		</br>
-
-		<label for="firstname">Prenom :</label>
-		<input type="text" name="firstname" value="" />
-		</br>
-
-		<label for="sex">Sexe :</label>
-		<input type="radio" id="rad1" name="sex" value="Male" />
-		<label for="rad1">M</label>
-		<input type="radio" id="rad2" name="sex" value="Female" />
-		<label for="rad2">F</label>
-		<?php echo form_error('sex'); ?>
-		</br>
-
-		<label for="BD">Date de naissance :</label>
-		<input type="date" name="BD" value="" />
-		<?php echo form_error('BD'); ?>
-		</br>
-
-		<label for="city">Ville :</label>
-		<input type="text" name="city" value=""/>
-		<?php echo form_error('city'); ?>
-		</br>
-
-		<label for="mail">E-mail :</label>
-		<input type="text" name="mail" value=""/>
-		<?php echo form_error('mail'); ?>
-		</br>
-
-		<input type="submit" value="Envoyer" />
-	</form>
+	</div>
+	<div id="wrap">
+		<div class="sign-page signup-page">
+			<div class="sign-title">
+				<h1>Inscrit toi sur Booda</h1>
+			</div>
+			<div>
+				<form action="" method="post" class="form">
+					<div class="form__row">
+						<div class="form__label">
+							<label for="pseudo">Pseudo</label>
+						</div>
+						<div class="form__field">
+							<input type="text" name="pseudo" class="input-text" value="<?php echo set_value('pseudo'); ?>" />
+						</div>
+						<?php echo form_error('pseudo'); ?>
+					</div>
+					<div class="form__row">
+						<div class="form__label">
+							<label for="mdp">Mot de passe</label>
+						</div>
+						<div class="form__field">
+							<input type="password" class="input-password" name="mdp" value="" />
+						</div>
+						<?php echo form_error('mdp'); ?>
+					</div>
+					<div class="form__row">
+						<div class="form__label">
+							<label for="name">Nom</label>
+						</div>
+						<div class="form__field">
+							<input type="text" class="input-text" name="name" value="" />
+						</div>
+					</div>
+					<div class="form__row">
+						<div class="form__label">
+							<label for="firstname">Prenom</label>
+						</div>
+						<div class="form__field">
+							<input type="text" class="input-text" name="firstname" value="" />
+						</div>
+					</div>
+					<div class="form__row">
+						<div class="form__label">
+							<label for="BD">Date de naissance</label>
+						</div>
+						<div class="form__field">
+							<input type="date" class="input-text" name="BD" value="" />
+						</div>
+						<?php echo form_error('BD'); ?>
+					</div>
+					<div class="form__row">
+						<div class="form__label">
+							<label for="city">Ville</label>
+						</div>
+						<div class="form__field">
+							<input type="text" class="input-text" name="city" value=""/>
+						</div>
+						<?php echo form_error('city'); ?>
+					</div>
+					<div class="form__row">
+						<div class="form__label">
+							<label for="sex">Sexe</label>
+						</div>
+						<div class="form__field" style="padding: 12px 10px;">
+								<label for="rad1">Homme</label>
+								<input type="radio" id="rad1" name="sex" value="Male" />
+								<label for="rad2">Femme</label>
+								<input type="radio" id="rad2" name="sex" value="Female" />
+						</div>
+						<?php echo form_error('sex'); ?>
+					</div>
+		
+					
+						<input type="submit" class="btn btn--orange btn--lg" value="Envoyer" />
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
