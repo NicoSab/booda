97bf0baebe3315f5d06459468962a22006af4b3b
Yayap <nicolas.sabella@epita.fr>
2014-05-11 01:22:58 +0200

    AJAX refresh for chat OK

--- application/controllers/Chat.php
+++ application/controllers/Chat.php
@@ -32,7 +32,7 @@ class Chat extends CI_Controller
 
 	public function conversation($convId)
 	{
-		$this->output->set_header("refresh:30;url=".base_url()."index.php/chat/conversation/".$convId); 
+		//$this->output->set_header("refresh:30;url=".base_url()."index.php/chat/conversation/".$convId); 
 
 		$userId = $this->session->userdata('userId');
 		$message = $this->input->post('message');
