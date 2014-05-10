a89ba0ac50680dffb755d7f430263614b9c3c8f8
Yayap <nicolas.sabella@epita.fr>
2014-05-10 20:59:19 +0200

    recup message ok

--- base_sql_booda.sql
+++ base_sql_booda.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Sam 10 Mai 2014 à 04:51
+-- Généré le: Sam 10 Mai 2014 à 19:12
 -- Version du serveur: 5.5.33
 -- Version de PHP: 5.5.3
 
@@ -23,6 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `Conversations`
+--
+
+CREATE TABLE `Conversations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser1` int(11) NOT NULL,
+  `idUser2` int(11) NOT NULL,
+  `lastUpdatedDate` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idUser1` (`idUser1`),
+  UNIQUE KEY `idUser2` (`idUser2`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Vider la table avant d'insérer `Conversations`
+--
+
+TRUNCATE TABLE `Conversations`;
+--
+-- Contenu de la table `Conversations`
+--
+
+INSERT INTO `Conversations` (`id`, `idUser1`, `idUser2`, `lastUpdatedDate`) VALUES
+(1, 1, 2, '2014-05-10');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `Messages`
 --
 
@@ -35,7 +63,7 @@ CREATE TABLE `Messages` (
   PRIMARY KEY (`id`),
   KEY `idUser` (`idUser`),
   KEY `idConversation` (`idConversation`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Vider la table avant d'insérer `Messages`
@@ -48,11 +76,109 @@ TRUNCATE TABLE `Messages`;
 
 INSERT INTO `Messages` (`id`, `idUser`, `idConversation`, `createdDate`, `message`) VALUES
 (1, 1, 1, '2014-05-10', '0'),
-(3, 1, 1, '2014-05-10', 'message 1');
+(3, 1, 1, '2014-05-10', 'message 1'),
+(5, 1, 1, '2014-05-10', '		test'),
+(6, 1, 1, '2014-05-10', 'lol'),
+(7, 1, 1, '2014-05-10', '		lk,'),
+(8, 1, 1, '2014-05-10', '		test\n'),
+(9, 1, 1, '2014-05-10', '		'),
+(10, 1, 1, '2014-05-10', '		'),
+(11, 1, 1, '2014-05-10', '		'),
+(12, 1, 1, '2014-05-10', '		lol');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Photos`
+--
+
+CREATE TABLE `Photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `idProfil` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idProfil` (`idProfil`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Vider la table avant d'insérer `Photos`
+--
+
+TRUNCATE TABLE `Photos`;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Profils`
+--
+
+CREATE TABLE `Profils` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Description` varchar(150) NOT NULL,
+  `Hobbies` varchar(100) NOT NULL,
+  `Interest` varchar(50) NOT NULL,
+  `MaritalSituation` varchar(50) NOT NULL,
+  `Sexuality` varchar(100) NOT NULL,
+  `Job` varchar(100) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idUser` (`idUser`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Vider la table avant d'insérer `Profils`
+--
+
+TRUNCATE TABLE `Profils`;
+--
+-- Contenu de la table `Profils`
+--
+
+INSERT INTO `Profils` (`id`, `Description`, `Hobbies`, `Interest`, `MaritalSituation`, `Sexuality`, `Job`, `idUser`) VALUES
+(1, '0', 'teste', 'Both', 'In couple', 'Heterosexuel', 'Etudiant', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Pass` varchar(50) NOT NULL,
+  `Firstname` varchar(50) NOT NULL,
+  `Lastname` varchar(50) NOT NULL,
+  `Sexe` varchar(25) NOT NULL,
+  `BirthDate` date NOT NULL,
+  `City` varchar(50) NOT NULL,
+  `Mail` varchar(100) NOT NULL,
+  `Pseudo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Vider la table avant d'insérer `Users`
+--
+
+TRUNCATE TABLE `Users`;
+--
+-- Contenu de la table `Users`
+--
+
+INSERT INTO `Users` (`id`, `Pass`, `Firstname`, `Lastname`, `Sexe`, `BirthDate`, `City`, `Mail`, `Pseudo`) VALUES
+(1, 'test', 'Nicolas', 'Sabella', 'Male', '2014-05-09', 'Le Perreux Sur Marne', 'test@test.com', 'Yayap'),
+(2, 'toto', '', '', 'Female', '2013-02-03', 'Villejuif', 'lolo@lolo.com', 'Test'),
+(6, 'bla', '', '', 'Female', '2014-05-09', 'Villejuif', 'toto@toto', 'Test');
 
 --
 -- Contraintes pour les tables exportées
 --
+
+--
+-- Contraintes pour la table `Conversations`
+--
+ALTER TABLE `Conversations`
+  ADD CONSTRAINT `conversations_ibfk_1` FOREIGN KEY (`idUser1`) REFERENCES `Users` (`id`),
+  ADD CONSTRAINT `conversations_ibfk_2` FOREIGN KEY (`idUser2`) REFERENCES `Users` (`id`);
 
 --
 -- Contraintes pour la table `Messages`
@@ -61,13 +187,18 @@ ALTER TABLE `Messages`
   ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `Users` (`id`),
   ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`idConversation`) REFERENCES `Conversations` (`id`);
 
+--
+-- Contraintes pour la table `Photos`
+--
+ALTER TABLE `Photos`
+  ADD CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`idProfil`) REFERENCES `Profils` (`id`);
+
+--
+-- Contraintes pour la table `Profils`
+--
+ALTER TABLE `Profils`
+  ADD CONSTRAINT `profils_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `Users` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-ALTER TABLE `booda`.`users` 
-CHANGE COLUMN `Pass` `Pass` VARCHAR(128) NOT NULL ;
-
-ALTER TABLE `booda`.`users` 
-ADD UNIQUE INDEX `Mail_UNIQUE` (`Mail` ASC);
-
