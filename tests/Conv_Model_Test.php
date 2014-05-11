69a28398c31fb4169e362950b7c59611fb480beb
Yayap <nicolas.sabella@epita.fr>
2014-05-11 23:05:13 +0200

    test for phpunit

--- tests/Conv_Model_Test.php
+++ tests/Conv_Model_Test.php
@@ -1,32 +1,17 @@
 <?php
 
-class Conv_Model_Test extends PHPUnit_Extensions_Database_TestCase { 	
+class Conv_Model_Test extends PHPUnit_Framework_TestCase { 	
 
-    /*private $CI; 	
+    private $CI; 	
     public function setUp()
     { 	
 		$this->CI = &get_instance();
 		$this->CI->load->model('ConvModel');
-		//$this->CI->load->database('booda');
 	}
 
 	public function testGetTenLastConversations() {
 		$convs = $this->CI->ConvModel->get_ten_last_conversation(7);
-		print_r($convs);
+		print_r($this->CI);
 		$this->assertEquals(1, count($convs));
-	}*/
-
-    public function getConnection()
-    {
-    }
-
-    /**
-     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
-     */
-    public function getDataSet()
-    {
-		$convs = $this->CI->ConvModel->get_ten_last_conversation(7);
-		print_r($convs);
-		$this->assertEquals(1, count($convs));
-    }
+	}
 }
